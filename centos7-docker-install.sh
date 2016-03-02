ce027fb1218af933b9cdb57934af0eebf460388f
Stanislav Blokhin <stanislav.blokhin@tomologic.com>
2016-03-02 22:03:09 +0100

    Change yum repository to Docker's own packaging
    Also release this software into public domain.

--- centos7-docker-install.sh
+++ centos7-docker-install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-cat <<EOT > /etc/yum.repos.d/virt7-testing.repo
-[virt7-testing]
-name=virt7-testing
-baseurl=http://cbs.centos.org/repos/virt7-testing/x86_64/os/
+cat <<EOT > /etc/yum.repos.d/docker-centos.repo
+[docker-centos]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/$releasever/
 enabled=1
-gpgcheck=0
-exclude=kernel
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
 EOT
 
 yum -y update
-yum -y install docker
+yum -y install docker-engine python-pip
+pip install docker-compose
 systemctl enable docker
 systemctl start docker
